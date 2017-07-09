604ae194dcb9d678a2a6efee06e3bf4a004d3a50
SebUndefined <me@sebundefined.fr>
2017-07-09 11:01:12 +0200

    Test start

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\AppBundle\Controller;
 
+use SebUndefined\ShopBundle\Services\CheckDate;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends KernelTestCase
 {
-    public function testIndex()
+    /*public function testIndex()
     {
         $client = static::createClient();
 
@@ -14,5 +16,36 @@ class DefaultControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+    }*/
+    private $em;
+    public function setUp()
+    {
+        self::bootKernel();
+        $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
+    }
+
+    public function testDate() {
+        $date = "22/10/2017";
+        echo "Testing date as String. String given : " . $date;
+        $serviceDate = new CheckDate($this->em);
+        $result = $serviceDate->isValidFormat($date);
+        $this->assertEquals(true, $result);
+
+    }
+    public function testIsNotBefore() {
+        $dateBefore = "22/10/2016";
+        echo "Testing date is not before. Date given : " . $dateBefore;
+        $serviceDate = new CheckDate($this->em);
+        $dateAsDateType = \DateTime::createFromFormat('d/m/Y', $dateBefore);
+        $result = $serviceDate->isNotBefore($dateAsDateType);
+        $this->assertEquals(false, $result);
+    }
+    public function testIsNotClosed() {
+        $date = "25/12/2017";
+        echo "Testing date is not closed : " . $date;
+        $serviceDate = new CheckDate($this->em);
+        $dateAsDateType = \DateTime::createFromFormat('d/m/Y', $date);
+        $result = $serviceDate->isNotAtClosedDay($dateAsDateType);
+        $this->assertEquals(true, $result);
     }
 }
